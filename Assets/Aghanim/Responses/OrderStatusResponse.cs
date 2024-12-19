aca16a8b465e557d7067c9f58bf32abdd4740487
Albert Tugushev <albert@aghanim.com>
2024-12-19 13:33:12 +0000

    Add order_id to OrderStatus

--- Assets/Aghanim/Responses/OrderStatusResponse.cs
+++ Assets/Aghanim/Responses/OrderStatusResponse.cs
@@ -6,6 +6,7 @@ namespace Aghanim.Responses
     [Serializable]
     public class OrderStatus
     {
+        public string order_id;
         public string item_sku;
         public bool is_success;
     }
